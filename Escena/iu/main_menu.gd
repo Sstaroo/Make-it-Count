9cfe5d19fac3928b5a5d303402326e2881000deb
Sstaroo <112453226+Sstaroo@users.noreply.github.com>
2022-09-04 22:26:41 -0400

    Menu
    Se creo un menu con los principales botones PLAY (lleva a la primera escena), CREDITS (hace print de "escuadron carpincho") y EXIT (finaliza el juego)

---
+++ Escena/iu/main_menu.gd
@@ -0,0 +1,17 @@
+extends MarginContainer
+onready var play = $VBoxContainer/Play
+onready var credits = $VBoxContainer/Credits
+onready var exit = $VBoxContainer/Exit
+
+func _ready():
+	play.connect("pressed",self,"_on_play_pressed")
+	credits.connect("pressed",self,"_on_credits_pressed")
+	exit.connect("pressed",self,"_on_exit_pressed")
+
+func _on_credits_pressed():
+	print ("Escuadron Carpincho")
+func _on_play_pressed():
+	get_tree().change_scene("res://Escena/main.tscn")
+
+func _on_exit_pressed():
+	get_tree().quit()
