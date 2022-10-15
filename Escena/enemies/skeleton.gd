0c5c517b6fd10733d18f1d26f783a41dd77e119b
Sstaroo <112453226+Sstaroo@users.noreply.github.com>
2022-10-15 12:17:02 -0300

    arreglado bug esqueleto
    ahora el esqueleto choca con los muros y se devuelve.
    tambien se devuelve antes de caer

--- Escena/enemies/skeleton.gd
+++ Escena/enemies/skeleton.gd
@@ -4,9 +4,8 @@ var velocity = Vector2()
 var SPEED = 35
 var GRAVITY = 10
 var direction = 1
+
 onready var Ray = $CollisionShape2D/RayCast2D
-# var a = 2
-# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,25 +14,19 @@ func _ready():
 func _physics_process(delta):
 	velocity.x = SPEED * direction
 	velocity.y += GRAVITY
-	velocity = move_and_slide(velocity)
-	
+	velocity = move_and_slide(velocity, Vector2.UP)
+
 	#Continuous movement
 	if direction == 1:
 		$AnimatedSprite.flip_h = false
 	else:
 		$AnimatedSprite.flip_h = true
-
-	
+	if is_on_floor():
 	#Stopping before falls
-	if Ray.is_colliding() == false:
-		direction = direction * -1
-		Ray.position.x *= -1
+		if not Ray.is_colliding():
+			direction = direction * -1
+			Ray.position.x *= -1
 	#Turning around when wall
-	if is_on_wall():
-		direction = direction * -1
-		Ray.position.x *= -1
-	
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		if is_on_wall():
+			direction = direction * -1
+			Ray.position.x *= -1
