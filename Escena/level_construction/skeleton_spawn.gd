f37c1e896f627ac147bedc5944892e670ad4a44f
Joshe890 <j_castro_kz@hotmail.com>
2022-12-14 16:38:19 -0300

    Level 1 completo
    - Se incluye parallax con diferentes colores
    - Se termina el nivel 1
    - Se agrega el portal para el boss

--- Escena/level_construction/skeleton_spawn.gd
+++ Escena/level_construction/skeleton_spawn.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 export (PackedScene) var skeleton
-export(bool) var autospawn
+export (bool) var autospawn
 export (float) var time_value
 export (float) var num_of_spawn
 
@@ -10,6 +10,11 @@ onready var is_active
 
 func _ready():
 	is_active = false
+	
+func  execute():
+	is_active = false
+	autospawn = false
+	
 
 func _on_skeleton_spawn_body_entered(body):
 
@@ -17,7 +22,6 @@ func _on_skeleton_spawn_body_entered(body):
 		is_active = true
 		spawner.scene = skeleton
 		if autospawn:
-			print("autospawn")
 			spawner.start(time_value)
 			num_of_spawn -= 1
 			
@@ -25,7 +29,7 @@ func _on_skeleton_spawn_body_entered(body):
 			spawner.spawn()
 			num_of_spawn -= 1
 		
-	if(num_of_spawn <= 0):
+	if num_of_spawn <= 0:
 		is_active = false
 		autospawn = false
 	
